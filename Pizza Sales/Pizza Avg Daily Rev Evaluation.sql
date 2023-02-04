ab9925f27de42d44b5921e39a90794f29fdc71fb
Jonathan Smith <122460643+johnzatopek@users.noreply.github.com>
2023-02-04 14:33:14 -0500

    Update Pizza Avg Daily Rev Evaluation.sql

--- Pizza Sales/Pizza Avg Daily Rev Evaluation.sql
+++ Pizza Sales/Pizza Avg Daily Rev Evaluation.sql
@@ -1,7 +1,7 @@
 /*Join tables to view when pizzas were ordered, what types of pizza, and the cost of each pizza ordered
 Need to round pizza price because decimal values go past $xx.xx */
 WITH orders_by_hour AS (
-SELECT o.order_id, date, DATETRUNC(hour,time) AS hour_ordered, p.pizza_id, ROUND(p.price,2) AS price_two_decimals
+SELECT o.order_id, od.quantity, date, DATETRUNC(hour,time) AS hour_ordered, p.pizza_id, ROUND(p.price,2) AS price_two_decimals
 FROM orders AS o
 LEFT JOIN order_details AS od
 ON o.order_id = od.order_id
@@ -10,7 +10,7 @@ ON od.pizza_id = p.pizza_id),
 
 --CTE returning revenue per day for every day of sales
 revenue_per_day AS (
-SELECT date, SUM(price_two_decimals) AS revenue_per_day
+SELECT date, SUM(price_two_decimals*quantity) AS revenue_per_day
 FROM orders_by_hour
 GROUP BY date
 )
