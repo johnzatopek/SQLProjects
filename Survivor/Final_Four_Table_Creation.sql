eb3718915167bc5d67112f9467b0752322646404
Jonathan Smith <122460643+johnzatopek@users.noreply.github.com>
2023-03-02 21:59:51 -0500

    Add files via upload

---
+++ Survivor/Final_Four_Table_Creation.sql
@@ -0,0 +1,14 @@
+--Create a table that contains the needed fields to analyze performance of the final four contestants in Survivor
+CREATE TABLE Final_Four (
+Season int
+, Season_Theme varchar(255)
+, Last_Name varchar(255)
+, First_Name varchar(255)
+, Gender varchar(255)
+, Age int
+, Win_Final_Four varchar(255)
+, Participate_in_Fire varchar(255)
+, Win_Fire varchar(255)
+, Final_Three varchar(255)
+, Win_Season varchar(255)
+, Votes_to_Win int)
